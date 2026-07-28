d2a221155b6663a0b16092fc1002aad52988f94e
Willie Shen <cg10250207@gmail.com>
2026-07-29 02:52:46 +0800

    y

--- update.sh
+++ update.sh
@@ -3,10 +3,7 @@ set -e
 
 cd ~/.config/nvim
 git reset --hard
-git submodule deinit --all --force
 git pull --rebase
-git submodule sync --recursive
-git submodule update --init --remote --recursive
 git clean -fd
 rm basic.vim || true
 wget https://raw.githubusercontent.com/Willie169/vim-config/refs/heads/main/basic.vim
