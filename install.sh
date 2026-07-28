6f18343759ce9484f6c808d9c1102618ff8f7f2b
Willie Shen <cg10250207@gmail.com>
2026-07-29 03:08:00 +0800

    y

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -e
 
 mkdir -p ~/.config
 git clone --depth=1 https://github.com/Willie169/nvim-config.git ~/.config/nvim
+cd ~/.config/nvim
 wget https://raw.githubusercontent.com/Willie169/vim-config/refs/heads/main/basic.vim
 echo "Installed successfully!"
 
