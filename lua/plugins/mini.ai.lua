347bcaae658ffa863ad1a42d0b6555ca6a4ac72b
Willie Shen <cg10250207@gmail.com>
2026-07-29 20:17:42 +0800

    l

--- lua/plugins/mini.ai.lua
+++ lua/plugins/mini.ai.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"nvim-mini/mini.ai",
 		version = false,
+		event = "VeryLazy",
 		opts = function()
 			local ai = require("mini.ai")
 			return {
--- lua/plugins/mini.pairs.lua
+++ lua/plugins/mini.pairs.lua
@@ -2,8 +2,12 @@ return {
 	{
 		"nvim-mini/mini.pairs",
 		version = false,
-		skip_ts = { "string" },
-		skip_unbalanced = true,
-		markdown = true,
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			skip_ts = { "string" },
+			skip_unbalanced = true,
+			markdown = true,
+		},
 	},
 }
