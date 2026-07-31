98d747f13b7a8bbf779e238669fefb2c2aa5d9b5
Willie Shen <cg10250207@gmail.com>
2026-07-31 09:10:28 +0800

    y

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,17 +1,15 @@
 vim.opt.runtimepath = vim.opt.runtimepath + "~/.local/share/nvim/site"
 
-if vim.fn.has("android") == 1 then
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "TSUpdate",
-		callback = function()
-			require("nvim-treesitter.parsers").latex.install_info = {
-				generate = false,
-				url = "https://github.com/Willie169/tree-sitter-latex",
-				location = "tree-sitter-latex",
-			}
-		end,
-	})
-end
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TSUpdate",
+	callback = function()
+		require("nvim-treesitter.parsers").latex.install_info = {
+			generate = false,
+			url = "https://github.com/Willie169/tree-sitter-latex",
+			location = "tree-sitter-latex",
+		}
+	end,
+})
 
 return {
 	"nvim-treesitter/nvim-treesitter",
