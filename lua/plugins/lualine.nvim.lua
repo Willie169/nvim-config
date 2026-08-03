d18bba5111a6492ca7f31832dcd5f69f4e39c778
Willie Shen <cg10250207@gmail.com>
2026-08-03 20:30:48 +0800

    2026-08-03T12:30:48+00:00

--- lua/plugins/lualine.nvim.lua
+++ lua/plugins/lualine.nvim.lua
@@ -13,11 +13,23 @@ return {
 			end
 		end
 
+		local trouble = require("trouble")
+		local symbols = trouble.statusline({
+			mode = "lsp_document_symbols",
+			groups = {},
+			title = false,
+			filter = { range = true },
+			format = "{kind_icon}{symbol.name:Normal}",
+			-- The following line is needed to fix the background color
+			-- Set it to the lualine section you want to use
+			hl_group = "lualine_c",
+		})
+
 		require("lualine").setup({
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { { "b:gitsigns_head", icon = "" }, { "diff", source = diff_source }, "diagnostics" },
-				lualine_c = { "selectioncount", "searchcount" },
+				lualine_c = { symbols.get, cond = symbols.has },
 				lualine_x = { "filename", "filetype", "lsp_status" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
