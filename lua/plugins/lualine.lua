b9dcf5126b302b43306dec906bbaf2bd3136b6cb
Willie Shen <cg10250207@gmail.com>
2026-08-03 13:51:32 +0800

    2026-08-03T05:51:32+00:00

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,44 +14,12 @@ return {
 				end
 			end
 
-			local custom_fname = require("lualine.components.filename"):extend()
-			local highlight = require("lualine.highlight")
-			local default_status_colors = { saved = "#228B22", modified = "#C70039" }
-
-			function custom_fname:init(options)
-				custom_fname.super.init(self, options)
-				self.status_colors = {
-					saved = highlight.create_component_highlight_group(
-						{ bg = default_status_colors.saved },
-						"filename_status_saved",
-						self.options
-					),
-					modified = highlight.create_component_highlight_group(
-						{ bg = default_status_colors.modified },
-						"filename_status_modified",
-						self.options
-					),
-				}
-				if self.options.color == nil then
-					self.options.color = ""
-				end
-			end
-
-			function custom_fname:update_status()
-				local data = custom_fname.super.update_status(self)
-				data = highlight.component_format_highlight(
-					vim.bo.modified and self.status_colors.modified or self.status_colors.saved
-				) .. data
-				return data
-			end
-
 			require("lualine").setup({
 				sections = {
 					lualine_a = { "mode" },
-					lualine_b = { { "b:gitsigns_head", icon = "" }, { "diff", source = diff_source } },
-					lualine_c = { custom_fname },
-					lualine_x = { keymap },
-					-- lualine_x = { "filetype" },
+					lualine_b = { { "b:gitsigns_head", icon = "" }, { "diff", source = diff_source }, "diagnostics" },
+					lualine_c = { "file_name" },
+					lualine_x = { "buffers" },
 					lualine_y = { "progress" },
 					lualine_z = { "location" },
 				},
