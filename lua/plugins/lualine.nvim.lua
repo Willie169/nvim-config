0d2f28513fd16062cad192bec96c158c8d6287e0
Willie Shen <cg10250207@gmail.com>
2026-08-03 20:03:36 +0800

    2026-08-03T12:03:36+00:00

--- lua/plugins/lualine.nvim.lua
+++ lua/plugins/lualine.nvim.lua
@@ -13,24 +13,14 @@ return {
 			end
 		end
 
-		local trouble = require("trouble")
-		local symbols = trouble.statusline({
-			mode = "lsp_document_symbols",
-			groups = {},
-			title = false,
-			filter = { range = true },
-			format = "{kind_icon}{symbol.name:Normal}",
-			hl_group = "lualine_c",
-		})
-
 		require("lualine").setup({
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { { "b:gitsigns_head", icon = "" }, { "diff", source = diff_source }, "diagnostics" },
-				lualine_c = { { symbols.get, cond = symbols.has } },
+				lualine_c = { "selectioncount", "searchcount" },
 				lualine_x = { "filename", "filetype", "lsp_status" },
-				lualine_y = { "selectioncount", "searchcount" },
-				lualine_z = { "progress", "location" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			},
 		})
 	end,
