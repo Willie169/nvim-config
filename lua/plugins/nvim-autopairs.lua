7ef7ae132bf8bb5ab501ae5fd3a36db8388e3688
Willie Shen <cg10250207@gmail.com>
2026-08-04 20:40:58 +0800

    2026-08-04T12:40:58+00:00

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -8,55 +8,61 @@ return {
 		local ts_conds = require("nvim-autopairs.ts-conds")
 
 		npairs.setup({ check_ts = true })
-		npairs.add_rules(
+		npairs.add_rules({
 			Rule("$", "$", { "tex", "latex" }),
+		})
+		npairs.add_rules({
 			Rule("$$", "$$", { "tex", "latex" }),
+		})
+		npairs.add_rules({
 			Rule("\\(", "\\)", { "tex", "latex" }),
+		})
+		npairs.add_rules({
 			Rule("\\[", "\\]", { "tex", "latex" }),
-			{
-				Rule("=", "")
-					:with_pair(cond.not_inside_quote())
-					:with_pair(function(opts)
-						local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
-						if last_char:match("[%w%=%s]") then
-							return true
-						end
-						return false
-					end)
-					:replace_endpair(function(opts)
-						local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
-						local next_char = opts.line:sub(opts.col, opts.col)
-						next_char = next_char == " " and "" or " "
-						if prev_2char:match("%w$") then
-							return "<bs> =" .. next_char
-						end
-						if prev_2char:match("%=$") then
-							return next_char
-						end
-						if prev_2char:match("=") then
-							return "<bs><bs>=" .. next_char
-						end
-						return ""
-					end)
-					:set_end_pair_length(0)
-					:with_move(cond.none())
-					:with_del(cond.none()),
-			},
-			{
-				Rule("{", "},", "lua")
-					:with_pair(cond.not_after_regex(","))
-					:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
-			},
-			{
-				Rule("'", "',", "lua")
-					:with_pair(cond.not_after_regex(","))
-					:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
-			},
-			{
-				Rule('"', '",', "lua")
-					:with_pair(cond.not_after_regex(","))
-					:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
-			}
-		)
+		})
+		npairs.add_rules({
+			Rule("=", "")
+				:with_pair(cond.not_inside_quote())
+				:with_pair(function(opts)
+					local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+					if last_char:match("[%w%=%s]") then
+						return true
+					end
+					return false
+				end)
+				:replace_endpair(function(opts)
+					local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+					local next_char = opts.line:sub(opts.col, opts.col)
+					next_char = next_char == " " and "" or " "
+					if prev_2char:match("%w$") then
+						return "<bs> =" .. next_char
+					end
+					if prev_2char:match("%=$") then
+						return next_char
+					end
+					if prev_2char:match("=") then
+						return "<bs><bs>=" .. next_char
+					end
+					return ""
+				end)
+				:set_end_pair_length(0)
+				:with_move(cond.none())
+				:with_del(cond.none()),
+		})
+		npairs.add_rules({
+			Rule("{", "},", "lua")
+				:with_pair(cond.not_after_regex(","))
+				:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+		})
+		npairs.add_rules({
+			Rule("'", "',", "lua")
+				:with_pair(cond.not_after_regex(","))
+				:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+		})
+		npairs.add_rules({
+			Rule('"', '",', "lua")
+				:with_pair(cond.not_after_regex(","))
+				:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+		})
 	end,
 }
