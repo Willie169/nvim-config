4128b02372313262101b3fe84da26a178163512a
Willie Shen <cg10250207@gmail.com>
2026-08-05 18:39:19 +0800

    2026-08-05T10:39:19+00:00

--- lua/plugins/catppuccin-nvim.lua
+++ lua/plugins/catppuccin-nvim.lua
@@ -1,12 +1,13 @@
-vim.cmd.colorscheme("catppuccin-nvim")
-
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
-	opts = {
-		flavour = "mocha",
-		transparent_background = true,
-		float = { transparent = true },
-	},
+	config = function()
+		required("catppuccin").setup({
+			flavour = "mocha",
+			transparent_background = true,
+			float = { transparent = true },
+		})
+		vim.cmd.colorscheme("catppuccin-nvim")
+	end,
 }
