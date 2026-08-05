f0dc4197075e9d63f1328bb8ea079361a657695c
Willie Shen <cg10250207@gmail.com>
2026-08-05 17:57:06 +0800

    2026-08-05T09:57:06+00:00

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -14,7 +14,9 @@ return {
 				:with_pair(cond.not_before_text("$")),
 		})
 		npairs.add_rules({
-			Rule("$$", "$$", { "tex", "latex", "plaintex" }):with_pair(cond.not_before_text("\\")),
+			Rule("$$", "$$", { "tex", "latex", "plaintex" })
+				:with_pair(cond.not_before_text("\\$"))
+				:with_pair(ts_conds.is_not_ts_node({ "code" })),
 		})
 		npairs.add_rules({
 			Rule("\\(", "\\)", { "tex", "latex", "plaintex" }),
