1751d256fa90b8e88d282e8ab0bb6c3552304f24
Willie Shen <cg10250207@gmail.com>
2026-08-05 17:09:25 +0800

    2026-08-05T09:09:25+00:00

--- lua/plugins/grug-far.nvim.lua
+++ lua/plugins/grug-far.nvim.lua
@@ -4,11 +4,18 @@ return {
 	opts = { headerMaxWidth = 80 },
 	keys = {
 		{
+			"<leader>sg",
+			function()
+				require("grug-far").open()
+			end,
+			mode = { "n", "x" },
+			desc = "grug-far",
+		},
+		{
 			"<leader>sr",
 			function()
-				local grug = require("grug-far")
 				local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-				grug.open({
+				require("grug-far").open({
 					transient = true,
 					prefills = {
 						filesFilter = ext and ext ~= "" and "*." .. ext or nil,
@@ -16,7 +23,44 @@ return {
 				})
 			end,
 			mode = { "n", "x" },
-			desc = "Search and Replace",
+			desc = "grug-far in current file type",
+		},
+		{
+			"<leader>sh",
+			function()
+				local search = vim.fn.getreg("/")
+				-- surround with \b if "word" search (such as when pressing `*`)
+				if search and vim.startswith(search, "\\<") and vim.endswith(search, "\\>") then
+					search = "\\b" .. search:sub(3, -3) .. "\\b"
+				elseif search and vim.startswith(search, "\\V") then
+					search = search:sub(3)
+				end
+				local inst = require("grug-far").open({
+					prefills = {
+						search = search,
+					},
+				})
+				inst:when_ready(function()
+					inst:goto_input("replacement")
+				end)
+			end,
+			mode = { "n", "x" },
+			desc = "grug-far using @/ or visual selection",
+		},
+		{
+			"<leader>sl",
+			function()
+				local opts = {}
+				local entry = require("grug-far").get_last_history_entry()
+				if entry ~= nil then
+					opts.prefills = entry
+					opts.engine = entry.engine
+					opts.replacementInterpreter = entry.replacementInterpreter
+				end
+				require("grug-far").open(opts)
+			end,
+			mode = { "n", "x" },
+			desc = "grug-far using last history entry",
 		},
 	},
 }
