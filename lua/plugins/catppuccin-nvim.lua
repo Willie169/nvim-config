4a1c8f7edd2f44589939081f294f3488a4b97190
Willie Shen <cg10250207@gmail.com>
2026-08-05 18:39:52 +0800

    2026-08-05T10:39:52+00:00

--- lua/plugins/catppuccin-nvim.lua
+++ lua/plugins/catppuccin-nvim.lua
@@ -3,7 +3,7 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		required("catppuccin").setup({
+		require("catppuccin").setup({
 			flavour = "mocha",
 			transparent_background = true,
 			float = { transparent = true },
