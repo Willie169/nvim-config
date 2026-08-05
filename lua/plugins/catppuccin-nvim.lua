86c1db5b2b7bd7b31f491fbe5e60a0fbf0c76e3f
Willie Shen <cg10250207@gmail.com>
2026-08-05 18:55:01 +0800

    2026-08-05T10:55:01+00:00

--- lua/plugins/catppuccin-nvim.lua
+++ lua/plugins/catppuccin-nvim.lua
@@ -5,6 +5,11 @@ return {
 	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha",
+			integrations = {
+				grug_far = true,
+				lsp_trouble = true,
+				which_key = true,
+			},
 		})
 		vim.cmd.colorscheme("catppuccin-nvim")
 	end,
