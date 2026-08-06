0e6b6d5c6f5831d820da38a9aba6d26e55453ce4
Willie Shen <cg10250207@gmail.com>
2026-08-06 22:56:48 +0800

    2026-08-06T14:56:48+00:00

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,11 +1,11 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "saghen/blink.lib" },
+	dependencies = { "L3MON4D3/LuaSnip" },
 	build = function()
 		require("blink.cmp").build():pwait()
 	end,
 	opts = {
-		keymap = { preset = "default" },
+		keymap = { preset = "super-tab" },
 		completion = {
 			keyword = { range = "full" },
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
