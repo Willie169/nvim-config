71941ff54f9d40133f73383710cbdec04b8cb38a
Willie Shen <cg10250207@gmail.com>
2026-08-06 23:23:01 +0800

    2026-08-06T15:23:01+00:00

---
+++ lua/plugins/vimtex.lua
@@ -0,0 +1,11 @@
+return {
+	"lervag/vimtex",
+	lazy = false,
+	config = function()
+		vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+		vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+	end,
+	keys = {
+		{ "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
+	},
+}
