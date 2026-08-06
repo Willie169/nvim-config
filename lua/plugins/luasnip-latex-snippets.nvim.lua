ef078f38a4db5deca16bdbed834e1de8e15bac4a
Willie Shen <cg10250207@gmail.com>
2026-08-06 23:48:43 +0800

    2026-08-06T15:48:43+00:00

---
+++ lua/plugins/luasnip-latex-snippets.nvim.lua
@@ -0,0 +1,7 @@
+return {
+	"iurimateus/luasnip-latex-snippets.nvim",
+	dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
+	config = function()
+		require("luasnip-latex-snippets").setup()
+	end,
+}
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,10 +2,17 @@ return {
 	"lervag/vimtex",
 	lazy = false,
 	config = function()
-		-- vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+		vim.g.vimtex_compiler_enabled = vim.fn.executable("latexmk") == 1 and 1 or 0
 		vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
 	end,
 	keys = {
 		{ "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
+		{
+			"<localleader>lt",
+			function()
+				return require("vimtex.fzf-lua").run()
+			end,
+			desc = "vimtex fzf-lua",
+		},
 	},
 }
