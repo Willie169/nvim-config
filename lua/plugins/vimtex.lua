02264adf7c4d42cbd15ebcc8bff93edf2fbb8216
Willie Shen <cg10250207@gmail.com>
2026-08-07 00:01:26 +0800

    2026-08-06T16:01:26+00:00

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,20 @@ return {
 	lazy = false,
 	config = function()
 		vim.g.vimtex_compiler_enabled = vim.fn.executable("latexmk") == 1 and 1 or 0
+		vim.g.vimtex_compiler_latexmk_engines = {
+			["_"] = "-xelatex",
+			pdfdvi = "-pdfdvi",
+			pdfps = "-pdfps",
+			pdflatex = "-pdf",
+			luatex = "-lualatex",
+			lualatex = "-lualatex",
+			xelatex = "-xelatex",
+			["context (pdftex)"] = "-pdf -pdflatex=texexec",
+			["context (luatex)"] = "-pdf -pdflatex=context",
+			["context (xetex)"] = "-pdf -pdflatex='texexec --xtx'",
+		}
 		vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+		vim.g.vimtex_syntax_enabled = 0
 	end,
 	keys = {
 		{ "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
