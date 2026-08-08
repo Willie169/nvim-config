1d2815a41596e8af2417db39644d357039a62ecd
Willie Shen <cg10250207@gmail.com>
2026-08-08 14:38:52 +0800

    2026-08-08T06:38:52+00:00

--- lua/plugins/lualine.nvim.lua
+++ lua/plugins/lualine.nvim.lua
@@ -27,12 +27,85 @@ return {
 
 		require("lualine").setup({
 			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { { "b:gitsigns_head", icon = "" }, { "diff", source = diff_source }, "diagnostics" },
-				lualine_c = { { symbols.get, cond = symbols.has } },
-				lualine_x = { "filename", "filetype", "lsp_status" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
+				lualine_a = {
+					{
+						"mode",
+						on_click = function()
+							require("grug-far").open({
+								transient = true,
+								prefills = {
+									paths = vim.fn.fnameescape(vim.fn.expand("%")),
+								},
+							})
+						end,
+					},
+				},
+				lualine_b = {
+					{
+						"b:gitsigns_head",
+						on_click = function()
+							vim.cmd("FzfLua git_status")
+						end,
+					},
+					{
+						"diff",
+						source = diff_source,
+						on_click = function()
+							vim.cmd("FzfLua git_diff")
+						end,
+					},
+					{
+						"diagnostics",
+						on_click = function()
+							vim.cmd("Trouble diagnostics toggle")
+						end,
+					},
+				},
+				lualine_c = {
+					{
+						symbols.get,
+						cond = symbols.has,
+						on_click = function()
+							vim.cmd("InspectTree")
+						end,
+					},
+				},
+				lualine_x = {
+					{
+						"filename",
+						on_click = function()
+							vim.cmd("FzfLua files")
+						end,
+					},
+					{
+						"filetype",
+						on_click = function()
+							vim.cmd("FzfLua registers")
+						end,
+					},
+					{
+						"lsp_status",
+						on_click = function()
+							vim.cmd("FzfLua command_history")
+						end,
+					},
+				},
+				lualine_y = {
+					{
+						"progress",
+						on_click = function()
+							vim.cmd("FzfLua search_history")
+						end,
+					},
+				},
+				lualine_z = {
+					{
+						"location",
+						on_click = function()
+							vim.cmd("FzfLua undotree")
+						end,
+					},
+				},
 			},
 		})
 	end,
