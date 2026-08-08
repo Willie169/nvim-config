f382aeb0d041cba37aeef5fdbd9628cfcf3986f8
Willie Shen <cg10250207@gmail.com>
2026-08-08 16:09:14 +0800

    2026-08-08T08:09:14+00:00

--- lua/plugins/match-delimiter.nvim.lua
+++ lua/plugins/match-delimiter.nvim.lua
@@ -1,6 +1,6 @@
 return {
 	"Willie169/match-delimiter.nvim",
-    config = function()
-        require("match-delimiter"),setup()
-    end
+	config = function()
+		require("match-delimiter").setup()
+	end,
 }
