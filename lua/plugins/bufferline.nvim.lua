eeac0dfe1348b0088c5868a9628ec0e18db67956
Willie Shen <cg10250207@gmail.com>
2026-08-09 19:45:27 +0800

    2026-08-09T11:45:27+00:00

--- lua/plugins/bufferline.nvim.lua
+++ lua/plugins/bufferline.nvim.lua
@@ -33,33 +33,33 @@ return {
 					delay = 200,
 					reveal = { "close" },
 				},
-			custom_areas = {
-				right = function()
-					local result = {}
-					local seve = vim.diagnostic.severity
-					local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
-					local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
-					local info = #vim.diagnostic.get(0, { severity = seve.INFO })
-					local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+				custom_areas = {
+					right = function()
+						local result = {}
+						local seve = vim.diagnostic.severity
+						local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+						local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+						local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+						local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
 
-					if error ~= 0 then
-						table.insert(result, { text = "  " .. error, link = "DiagnosticError" })
-					end
+						if error ~= 0 then
+							table.insert(result, { text = "  " .. error, link = "DiagnosticError" })
+						end
 
-					if warning ~= 0 then
-						table.insert(result, { text = "  " .. warning, link = "DiagnosticWarn" })
-					end
+						if warning ~= 0 then
+							table.insert(result, { text = "  " .. warning, link = "DiagnosticWarn" })
+						end
 
-					if hint ~= 0 then
-						table.insert(result, { text = "  " .. hint, link = "DiagnosticHint" })
-					end
+						if hint ~= 0 then
+							table.insert(result, { text = "  " .. hint, link = "DiagnosticHint" })
+						end
 
-					if info ~= 0 then
-						table.insert(result, { text = "  " .. info, link = "DiagnosticInfo" })
-					end
-					return result
-				end,
-			},
+						if info ~= 0 then
+							table.insert(result, { text = "  " .. info, link = "DiagnosticInfo" })
+						end
+						return result
+					end,
+				},
 			},
 			highlights = require("catppuccin.special.bufferline").get_theme(),
 		})
