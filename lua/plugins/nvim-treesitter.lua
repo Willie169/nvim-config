2391339df2d94364dd1e7d113f3738efd8e921d0
Willie Shen <cg10250207@gmail.com>
2026-08-10 20:05:21 +0800

    2026-08-10T12:05:21+00:00

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -93,8 +93,6 @@ return {
 			-- For more info on folds see `:help folds`
 			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 			vim.wo.foldmethod = "expr"
-			vim.opt.foldenable = true
-			vim.opt.foldlevel = 99
 
 			-- Check if treesitter indentation is available for this language, and if so enable it
 			-- in case there is no indent query, the indentexpr will fallback to the vim's built in one
