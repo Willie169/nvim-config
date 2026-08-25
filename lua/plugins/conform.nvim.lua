f410abb95de869210275476dafec05da6695e16f
Willie Shen <cg10250207@gmail.com>
2026-08-25 17:34:49 +0800

    2026-08-25T09:34:49+00:00

--- lua/plugins/conform.nvim.lua
+++ lua/plugins/conform.nvim.lua
@@ -1,5 +1,6 @@
 return {
 	"stevearc/conform.nvim",
+	lazy = false,
 	keys = {
 		{
 			"<leader>cf",
@@ -10,38 +11,44 @@ return {
 			desc = "Format",
 		},
 	},
-	config = function()
-		require("conform").setup({
-			formatters = {
-				["clang-format"] = {
-					append_args = { "-style=llvm" },
-				},
-				shfmt = {
-					append_args = { "-i", "2", "-ci" },
-				},
-				autopep8 = {
-					append_args = { "--aggressive", "--aggressive" },
-				},
+	opts = {
+		default_format_opts = {
+			timeout_ms = 3000,
+			async = false,
+			quiet = false,
+			lsp_format = "fallback",
+		},
+		formatters = {
+			["clang-format"] = {
+				append_args = { "-style=llvm" },
 			},
-			formatters_by_ft = {
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-				java = { "clang-format" },
-				css = { "prettier" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				sh = { "shfmt" },
-				rust = { "rustfmt" },
-				lua = { "stylua" },
-				python = { "autopep8" },
+			shfmt = {
+				append_args = { "-i", "2", "-ci" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
+			autopep8 = {
+				append_args = { "--aggressive", "--aggressive" },
 			},
-		})
+		},
+		formatters_by_ft = {
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+			java = { "clang-format" },
+			css = { "prettier" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			sh = { "shfmt" },
+			rust = { "rustfmt" },
+			lua = { "stylua" },
+			python = { "autopep8" },
+		},
+		format_on_save = {
+			timeout_ms = 3000,
+			lsp_format = "fallback",
+		},
+	},
+	init = function()
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
 }
